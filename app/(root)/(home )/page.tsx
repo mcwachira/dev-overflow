8c4aa67cf7d434b1721095697f96b5deac9920aa
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-11-30 11:05:56 +0300

    Feature:Setup authentication using clerk

--- app/(root)/(home )/page.tsx
+++ app/(root)/(home )/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold">Nextjs 14 we are comming</h1>
-      <h2 className="h2-bold">Nextjs 14 we are comming</h2>
-      <h3 className="h3-bold">Nextjs 14 we are comming</h3>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
